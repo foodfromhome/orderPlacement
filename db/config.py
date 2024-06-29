6db6d00050d5843f72b1883e07ec333a55e052cb
Mekan777-alt <mekan2678@gmail.com>
2024-06-29 11:10:40 +0300

    update DATABASE_URL

--- db/config.py
+++ db/config.py
@@ -2,10 +2,8 @@ from config import settings
 import motor.motor_asyncio
 
 
-# DATABASE_URL = (f"mongodb://{settings.mongo_username}:{settings.mongo_password}@"
-#                 f"{settings.mongo_host}:{settings.mongo_port}")
-
-DATABASE_URL = f"mongodb://localhost:27017"
+DATABASE_URL = (f"mongodb://{settings.mongo_username}:{settings.mongo_password}@"
+                f"{settings.mongo_host}:{settings.mongo_port}")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
     DATABASE_URL, uuidRepresentation="standard"
